1d3a47d7105abdb958aab4fb6390cc02eddf36ac
Job104 <registercsie105@gmail.com>
2018-03-26 22:41:20 +0800

    add test folder

--- test/js/myjs.js
+++ test/js/myjs.js
@@ -1,7 +1,8 @@
-var count = 0;
 var idlist = [];
+var leftlist = [];
 var timelist = [0,1,3,6,8,12];
 var big = 0;
+var total = 0;
 
 $("#rightbtn").hide();
 
@@ -9,16 +10,24 @@ $("img").click(function(){
 	if(idlist.indexOf(this.id)==-1 && idlist.length<3)
 	{
 		idlist.push(this.id);
-		count++;
 		$( this ).animate({
 			top: "+=100"
 		}, 1000, function() {
 			
 		});
 	}
+	else if(idlist.indexOf(this.id)!=-1)
+	{
+		idlist.splice(idlist.indexOf(this.id),1);
+		$( this ).animate({
+			top: "-=100"
+		}, 1000, function() {
+			
+		});
+	}
 });
 
-function move(h,s,add_minus)
+function move(h,s,add_minus,push_remove)
 {
 	if(idlist.length>1 && idlist.indexOf("light")!=-1)
 	{
@@ -27,6 +36,15 @@ function move(h,s,add_minus)
 
 		for(var i=0;i<idlist.length;i++)
 		{
+			if(push_remove == "push" && idlist[i]!="light")
+			{
+				leftlist.push(idlist[i]);
+			}
+			else if(push_remove == "remove" && idlist[i]!="light")
+			{
+				leftlist.splice(leftlist.indexOf(idlist[i]),1);
+			}
+
 			var num = timelist[parseInt(idlist[i].replace("pic",""))];
 			if(num > big)
 			{
@@ -42,6 +60,7 @@ function move(h,s,add_minus)
 		}
 
 		$("#log").append("<p> 花費了" + big + "秒！</p>");
+		total += big;
 		idlist = [];
 		big = 0;
 	}
@@ -49,12 +68,17 @@ function move(h,s,add_minus)
 
 $("#leftbtn").click(function()
 {
-	move("leftbtn","rightbtn","-");
+	move("leftbtn","rightbtn","-","push");
+	if(leftlist.length==5)
+	{
+		alert("你總共花費了 "+total+" 秒...");
+	}
+	console.log(leftlist);
 });
-
 
 
 $("#rightbtn").click(function()
 {
-	move("rightbtn","leftbtn","+");
+	move("rightbtn","leftbtn","+","remove");
+	console.log(leftlist);
 });
